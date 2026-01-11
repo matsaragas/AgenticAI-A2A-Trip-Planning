1fbdefe26b7a408357bc5d2392dc92314a22b5bc
Petros Ypsilantis <pet_yps@hotmail.com>
2026-01-11 17:11:14 +0000

    add the search agent using google search

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ in a particular destination.
 
 
 
+Part of the code and ideas can be found [here](https://github.com/a2aproject/a2a-samples/tree/main/samples/python/agents/airbnb_planner_multiagent). However, original code has been modified to
+to make it work in scenarios where users have multiple queries, where each query can require 
+access to different remote agents. The proposed Agentic AI system has been enhance with an additional agent 
+that performs Google search to identify and propose venues to visit and activities to do in a country/city.
+
--- src/weather_agents/weather_agent.py
+++ src/weather_agents/weather_agent.py
@@ -17,7 +17,10 @@ def create_weather_agent() -> LlmAgent:
         model=Gemini(model="gemini-2.5-flash-lite"),
         name='weather_agent',
         description='An agent that can help questions about weather',
-        instruction="""You are a specialized weather forecast assistant. Your primary function is to utilize the provided tools to retrieve and relay weather information in response to user queries. You must rely exclusively on these tools for data and refrain from inventing information. Ensure that all responses include the detailed output from the tools used and are formatted in Markdown""",
+        instruction="""You are a specialized weather forecast assistant. Your primary function is to utilize the 
+        provided tools to retrieve and relay weather information in response to user queries. You must rely 
+        exclusively on these tools for data and refrain from inventing information. Ensure that all responses include 
+        the detailed output from the tools used and are formatted in Markdown""",
         tools=[
             MCPToolset(
                 connection_params=StdioServerParameters(
